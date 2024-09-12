d5e8fc9bffacfc36d6261e04d7a59b61a69a8e97
Wrf13121990@ <wagner.felippette@etec.sp.gov.br>
2024-09-11 22:19:45 -0300

    cadastro de produto completo

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Produto;
 use Illuminate\Http\Request;
 
 class ProdutoController extends Controller
 {
-    //
+    public function criar(Request $request) {
+        $produto = new Produto;
+        $produto->nome = $request->nome;
+        $produto->descricao = $request->preco;
+        $produto->preco = $request->preco;
+
+        $produto->save();
+        return redirect('/listar_produtos');
+    }
+
+    public function listar() {
+        $produtos = Produto::all();
+
+        return view("produtos", ["produtos"=>$produtos]);
+    }
+    public function formCriarProduto() {
+        return view("cadastro_produto");
+    }
 }
